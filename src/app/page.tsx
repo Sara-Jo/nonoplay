d7acae3b475443a82083fd3d1c317e65c5dadc2f
Sara-Jo <sarajo.dev@gmail.com>
2024-08-04 14:44:16 +0900

    add: mode toggle button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@
 
 import { useState } from "react";
 import styles from "./page.module.css";
+import SquareRoundedIcon from "@mui/icons-material/SquareRounded";
+import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
 
 type CellState = "filled" | "crossed" | null;
 type GridState = CellState[][];
@@ -10,16 +12,39 @@ export default function Home() {
   const [grid, setGrid] = useState<GridState>(
     Array(10).fill(Array(10).fill(null))
   );
+  const [mode, setMode] = useState<"fill" | "exclude">("fill");
+
+  const toggleMode = () => {
+    setMode((prevMode) => (prevMode === "fill" ? "exclude" : "fill"));
+  };
 
   return (
-    <div className={styles.grid}>
-      {grid.map((row, rowIndex) => (
-        <div key={rowIndex} className={styles.row}>
-          {row.map((cell, colIndex) => (
-            <div key={colIndex} className={styles.cell}></div>
-          ))}
+    <div className={styles.main}>
+      <div className={styles.grid}>
+        {grid.map((row, rowIndex) => (
+          <div key={rowIndex} className={styles.row}>
+            {row.map((cell, colIndex) => (
+              <div key={colIndex} className={styles.cell}></div>
+            ))}
+          </div>
+        ))}
+      </div>
+
+      <div className={styles.toggleButton} onClick={toggleMode}>
+        <div
+          className={`${styles.toggleSwitch} ${
+            mode === "fill" ? styles.fill : styles.exclude
+          }`}
+        >
+          {mode === "fill" ? <CloseRoundedIcon /> : <SquareRoundedIcon />}
         </div>
-      ))}
+        <div className={styles.toggleIcon}>
+          <CloseRoundedIcon sx={{ color: "black" }} />
+        </div>
+        <div className={styles.toggleIcon}>
+          <SquareRoundedIcon sx={{ color: "black" }} />
+        </div>
+      </div>
     </div>
   );
 }
